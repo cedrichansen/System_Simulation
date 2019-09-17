ad4a77edb013670e12e96e7cb23fa414fc7dc32b
Cedric Hansen <cedrichansen@Cedrics-MacBook-Air.local>
2019-09-17 15:47:27 -0400

    The vending machines now dispense coffee. Need to fix a few minor bugs/special cases, and change the way input is handled. wait -> w, cancel -> and process multiple commands at once

--- csc454hw02/src/main/java/VendingMachine.java
+++ csc454hw02/src/main/java/VendingMachine.java
@@ -5,17 +5,16 @@ public class VendingMachine {
     static int NICKEL_VALUE = 5;
     static int COFFEE_PRICE = 100;
 
-
     int numberOfNickels;
     int numberOfDimes;
     int numberOfQuarters;
 
+    int tikNumber = 0;
 
-    //upon creation of the vending machine, the customer has not put in any coins;
+    // upon creation of the vending machine, the customer has not put in any coins;
     float customerValue = 0;
 
     boolean changeButtonPressed = false;
-
 
     public VendingMachine(int initialNickels, int initialDimes, int initialQuarters) {
 
@@ -25,62 +24,73 @@ public class VendingMachine {
 
     }
 
+    public void dispenseCoffee() {
+        int numberOfCoffees = (int) ((customerValue) / COFFEE_PRICE);
 
-    public void dispenseCoffe() {
-        customerValue -= COFFEE_PRICE;
-        System.out.println("Dispensed coffee! Balance is now: " + customerValue);
+        if (numberOfCoffees > 0) {
+            customerValue -= numberOfCoffees * COFFEE_PRICE;
+            System.out.println("Dispensed " + numberOfCoffees + " coffee(s)! Balance is now: " + customerValue/100);
+        }
+
     }
 
     public void getChange() {
-        float amountToDispense = customerValue;
-        float amountDispensed = 0;
-        int numberOfQuartersToDispense = 0;
-        int numberOfDimesToDispense = 0;
-        int numberOfNickelsToDispense = 0;
 
+        if (changeButtonPressed) {
+            float amountToDispense = customerValue;
+            float amountDispensed = 0;
+            int numberOfQuartersToDispense = 0;
+            int numberOfDimesToDispense = 0;
+            int numberOfNickelsToDispense = 0;
 
-        if (amountToDispense >= QUARTER_VALUE) {
-            numberOfQuartersToDispense = (int) (amountToDispense / QUARTER_VALUE);
-            if (numberOfQuartersToDispense > numberOfQuarters) { //we do not have enough quarters left to give quarters, so give out as many as possible
-                numberOfQuartersToDispense = numberOfQuarters;
+            if (amountToDispense >= QUARTER_VALUE) {
+                numberOfQuartersToDispense = (int) (amountToDispense / QUARTER_VALUE);
+                if (numberOfQuartersToDispense > numberOfQuarters) { // we do not have enough quarters left to give
+                                                                     // quarters, so give out as many as possible
+                    numberOfQuartersToDispense = numberOfQuarters;
+                }
+                amountToDispense -= (QUARTER_VALUE * numberOfQuartersToDispense);
+                amountDispensed += (QUARTER_VALUE * numberOfQuartersToDispense);
             }
-            amountToDispense -= (QUARTER_VALUE * numberOfQuartersToDispense);
-            amountDispensed += (QUARTER_VALUE * numberOfQuartersToDispense);
+
+            if (amountToDispense >= DIME_VALUE) {
+                numberOfDimesToDispense = (int) (amountToDispense / DIME_VALUE);
+                if (numberOfDimesToDispense > numberOfDimes) {
+                    numberOfDimesToDispense = numberOfDimes;
+                }
+                amountToDispense -= (DIME_VALUE * numberOfDimesToDispense);
+                amountDispensed += (DIME_VALUE * numberOfDimesToDispense);
+            }
+
+            if (amountToDispense >= NICKEL_VALUE) {
+                numberOfNickelsToDispense = (int) (amountToDispense / NICKEL_VALUE);
+                if (numberOfNickelsToDispense > numberOfNickels) {
+                    numberOfNickelsToDispense = numberOfNickels;
+                }
+                amountToDispense -= (NICKEL_VALUE * numberOfNickelsToDispense);
+                amountDispensed += (NICKEL_VALUE * numberOfNickelsToDispense);
+            }
+
+            numberOfNickels -= numberOfNickelsToDispense;
+            numberOfDimes -= numberOfDimesToDispense;
+            numberOfQuarters -= numberOfQuartersToDispense;
+
+            assert (amountDispensed == customerValue);
+            assert (amountToDispense == 0);
+
+            customerValue -= amountDispensed;
+
+            assert (customerValue == 0);
+
+            System.out.println("Change has been returned: " + numberOfQuartersToDispense + " Quarters, "
+                    + numberOfDimesToDispense + " Dimes, " + numberOfNickelsToDispense
+                    + " Nickels. Total value returned: "
+                    + calculateValue(numberOfNickelsToDispense, numberOfDimesToDispense, numberOfQuartersToDispense)
+                            / 100);
+
+            changeButtonPressed = false;
         }
 
-        if (amountToDispense >= DIME_VALUE) {
-            numberOfDimesToDispense = (int) (amountToDispense / DIME_VALUE);
-            if (numberOfDimesToDispense > numberOfDimes) {
-                numberOfDimesToDispense = numberOfDimes;
-            }
-            amountToDispense -= (DIME_VALUE * numberOfDimesToDispense);
-            amountDispensed += (DIME_VALUE * numberOfDimesToDispense);
-        }
-
-        if (amountToDispense >= NICKEL_VALUE) {
-            numberOfNickelsToDispense = (int) (amountToDispense / NICKEL_VALUE);
-            if (numberOfNickelsToDispense > numberOfNickels) {
-                numberOfNickelsToDispense = numberOfNickels;
-            }
-            amountToDispense -= (NICKEL_VALUE * numberOfNickelsToDispense);
-            amountDispensed += (NICKEL_VALUE * numberOfNickelsToDispense);
-        }
-
-
-        numberOfNickels -= numberOfNickelsToDispense;
-        numberOfDimes -= numberOfDimesToDispense;
-        numberOfQuarters -= numberOfQuartersToDispense;
-
-        assert (amountDispensed == customerValue);
-        assert (amountToDispense == 0);
-
-        customerValue -= amountDispensed;
-
-        assert (customerValue == 0);
-
-        System.out.println("Change has been returned: " + numberOfQuartersToDispense + " Quarters, " +
-                numberOfDimesToDispense + " Dimes, " + numberOfNickelsToDispense + " Nickels. Total value returned: " +
-                calculateValue(numberOfNickelsToDispense, numberOfDimesToDispense, numberOfQuartersToDispense)/100);
     }
 
     private float calculateValue(int nickels, int dimes, int quarters) {
@@ -90,7 +100,8 @@ public class VendingMachine {
     }
 
     public void startSimulation() {
-        //allow people to put in money and press buttons and stuff... Basically the loop that processes the sim
+        // allow people to put in money and press buttons and stuff... Basically the
+        // loop that processes the sim
 
         String command = "";
         Main.s.nextLine(); // flush buffer again;
@@ -107,43 +118,49 @@ public class VendingMachine {
     }
 
     private void processAction(String command) {
+
+        tikNumber++;
+
+        dispenseCoffee();
+        getChange();
         // possible inputs: nickel, dime, quarter, cancel, wait
 
-        int quartersInserted = (int)command.chars().filter(ch -> ch == 'q').count();
-        int dimesInserted = (int)command.chars().filter(ch -> ch == 'd').count();
-        int nickelsInserted = (int)command.chars().filter(ch -> ch == 'n').count();
+        int quartersInserted = (int) command.chars().filter(ch -> ch == 'q').count();
+        int dimesInserted = (int) command.chars().filter(ch -> ch == 'd').count();
+        int nickelsInserted = (int) command.chars().filter(ch -> ch == 'n').count();
         int coinsInserted = (quartersInserted + dimesInserted + nickelsInserted);
 
-        if (command.equals("wait")) {
+        if (command.equals("w")) {
             System.out.println("waiting for next tik... ");
             System.out.println(this.toString());
 
-        } else if (command.equals("cancel")){
-            //TODO ask if cancel means that the change button was pressed?
-            if (customerValue > 0) {
-                getChange();
-            } else {
-                System.out.println("No money has been inserted! No change to be given");
-            }
-            printBalance();
-
+        } else if (command.equals("c")) {
+            // TODO ask if cancel means that the change button was pressed?
+            System.out.println("change button pressed. Change will be dispensed on next tik");
+            changeButtonPressed = true;
 
         } else if (command.length() == coinsInserted) {
-            //if all things we typed were valid coins, then process them.
+            // if all things we typed were valid coins, then process them.
             addCoins(nickelsInserted, dimesInserted, quartersInserted);
-            System.out.println("Added: " + nickelsInserted + " nickels, " + dimesInserted + " dimes, " + quartersInserted + " quarters\n" +
-                    "Total added: " + calculateValue(nickelsInserted, dimesInserted, quartersInserted)/100);
+            System.out.println("Added: " + nickelsInserted + " nickels, " + dimesInserted + " dimes, "
+                    + quartersInserted + " quarters\n" + "Total added: "
+                    + calculateValue(nickelsInserted, dimesInserted, quartersInserted) / 100);
             printBalance();
 
         } else {
-            System.out.println("Unknown input.... please type a valid input");
+            if (!command.equals("exit")) {
+                System.out.println("Unknown input.... please type a valid input");
+            } else {
+                System.out.println("Closing the vending machine...");
+            }
         }
+
+        System.out.println();
 
     }
 
-
-    void addCoins(int nickels, int dimes, int quarters){
-        numberOfQuarters +=quarters;
+    void addCoins(int nickels, int dimes, int quarters) {
+        numberOfQuarters += quarters;
         numberOfDimes += dimes;
         numberOfNickels += nickels;
 
@@ -152,12 +169,12 @@ public class VendingMachine {
     }
 
     public String toString() {
-        return "Nickels: " + numberOfNickels + " Dimes: " + numberOfDimes + " Quarters: " + numberOfQuarters + "\nBalance: " + customerValue/100;
+        return "Nickels: " + numberOfNickels + " Dimes: " + numberOfDimes + " Quarters: " + numberOfQuarters
+                + "\nBalance: " + customerValue / 100 + " current tik " + tikNumber;
     }
 
-    public void printBalance(){
-        System.out.println("Balance: " + customerValue/100);
+    public void printBalance() {
+        System.out.println("Balance: " + customerValue / 100);
     }
-
 
 }
