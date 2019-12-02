f53d8692b66c2a2cbc44274cc87fc5eff36efa68
cedrichansen <ceddy29@yahoo.ca>
2019-12-02 17:01:30 -0500

    basically working, just need to iron out a tiny little issue with ending the simulation

--- csc454hw07/src/main/java/Dealership.java
+++ csc454hw07/src/main/java/Dealership.java
@@ -1,6 +1,6 @@
 public class Dealership extends Model<Car, Car> {
 
-    static final int TIME_TO_SELL_CAR = 10;
+    static final int TIME_TO_SELL_CAR = 25;
     double timeRemainingOnCurrentSale;
 
     int numberOfCarsToSell;
@@ -17,36 +17,59 @@ public class Dealership extends Model<Car, Car> {
 
     @Override
     public String lambda() {
-        return null;
+        this.out.currentValue = new Car();
+        numberOfCarsToSell--;
+        return "Dealership just sold a car!";
     }
 
     @Override
-    public void externalTransition(Time elapsedTime, String in) {
+    public void externalTransition(Time currentTime, String in) {
+        if (numberOfCarsToSell > 0 ) {
+            Time elapsed = new Time(currentTime.realTime - lastKnownTime.realTime, 0);
+            timeRemainingOnCurrentSale -= elapsed.realTime;
+        } else {
+            timeRemainingOnCurrentSale = TIME_TO_SELL_CAR;
+        }
 
+        Car c = this.in[0].currentValue;
+        if (c != null) {
+            this.numberOfCarsToSell++;
+            this.in[0].currentValue = null;
+        }
+
+        lastKnownTime = currentTime;
     }
 
     @Override
     public void internalTransition() {
-
+        this.lastKnownTime = new Time(this.lastKnownTime.realTime + timeRemainingOnCurrentSale, 0); // we might care to know the time at which processing time was reset
+        this.timeRemainingOnCurrentSale = TIME_TO_SELL_CAR;
     }
 
     @Override
-    public void confluentTransition(Time elapsedTime, String in) {
-
+    public void confluentTransition(Time currentTime, String in) {
+        internalTransition();
+        lastKnownTime = currentTime; //this is to prevent the model from thinking it is already done another part, since internal transition reset time
+        externalTransition(currentTime, in);
     }
 
     @Override
     public Time timeAdvance() {
-        return null;
+        if (numberOfCarsToSell == 0) {
+            return new Time(Double.MAX_VALUE, 0);
+        } else {
+            return new Time(timeRemainingOnCurrentSale, 0);
+        }
     }
 
     @Override
     public double getMaxTimeAdvance() {
-        return 0;
+        return Double.MAX_VALUE;
     }
 
     @Override
     public String toString() {
-        return null;
+        return "Car dealership - cars to sell: " + this.numberOfCarsToSell + " time left on sale: " + timeRemainingOnCurrentSale ;
+
     }
 }
